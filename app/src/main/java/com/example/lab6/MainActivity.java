2ab4592d37c568fc5b067562c9f3ca08868a0c21
przemo7873 <62542229+przemo7873@users.noreply.github.com>
2020-06-17 10:49:57 +0200

    Config

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gameList(View v)
+    {
+        Intent intencja = new Intent(
+                getApplicationContext(),
+                GamesList.class);
+        intencja.putExtra("gra", v.getId());
+        startActivity(intencja);
+    }
+
 }
